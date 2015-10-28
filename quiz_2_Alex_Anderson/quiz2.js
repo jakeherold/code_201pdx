c25506c3af9b6d25b8d1d1cebd9e4b26c8d1f3d2
andalex <and4alex@gmail.com>
2015-10-28 13:38:53 -0700

    scratchSpace

---
+++ quiz_2_Alex_Anderson/quiz2.js
@@ -0,0 +1,18 @@
+function funkyFunction(some_num, some_num_2) {
+    var sum_num = 4;
+    var sum_num_2 = 20;
+    return sum_num * sum_num_2;
+    }
+console.log(funkyFunction(4));
+
+function funkyFunction2(another_num) {
+    var another_num = 6;
+    return another_num * 4;
+    }
+console.log(funkyFunction2());
+
+function funkyFunction3(some_different_num) {
+    var some_different_num = 3;
+    return some_different_num * 5;
+    }
+console.log(funkyFunction3());
