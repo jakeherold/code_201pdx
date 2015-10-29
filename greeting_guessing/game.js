71a45defc0f8be76e127d38ba6fb77a708cc0b8e
Jacob Porter Herold <dcejake@gmail.com>
2015-10-29 15:07:41 -0700

    Thoroughly messed up: array reconfig attempt

--- greeting_guessing/game.js
+++ greeting_guessing/game.js
@@ -1,15 +1,34 @@
-var questions =
-["Does the refrigerator light really turn off when you close the door?",
-"Is schrodinger's cat alive or dead?",
-"Can you cook a steak by dropping it?", "Thanks, you got " + num_correct + "/3 right!"];
-
-var question_num = 0;
+//var question_num = 0;
 var num_correct = 0;
+
+//Array of question/answer objects
+
+var questions =[
+"Does the refrigerator light really turn off when you close the door?",
+"Is schrodinger's cat alive or dead?",
+"Can you cook a steak by dropping it?"
+
+];
+
+var answers =[
+"yes",
+"both",
+"yes"
+];
+
+
 
 
 function questionFunction () {
-  if (question_num < questions.length)document.getElementById('question').innerHTML =  questions[question_num]
-  question_num += 1;
+  for (ii = 0; ii < questions.length; ii++)(function(ii){questions{
+
+      if (question_num <= questions.length){
+
+        document.getElementById('question').innerHTML = questions[ii];
+        console.log(ii);
+          //question_num += 1;
+    //}
+  }
 }
 
 
@@ -20,10 +39,10 @@ var result = document.getElementById('result').value;
     if ((result === "yes")||(result === "y")) {
       document.getElementById('answer').innerHTML = "Sure we can be fairly certain about that.";
       document.getElementById('question').innerHTML = "";
-      num_correct += 1
+      num_correct += 1;
     }
     else {
-      document.getElementById('answer').innerHTML = "I guess thats an alright answer.";
+      document.getElementById('answer').innerHTML = "I guess thats an alright answer, though its not the one we're looking for.";
     }
 //alerts the user with a summary statement telling them how many /3 they got correct
     }
